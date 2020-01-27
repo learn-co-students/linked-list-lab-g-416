7011a1c44fa4afc552c32858d4be19c56893bff2
radville <laura.radville@gmail.com>
2020-01-27 14:48:32 -0500

    Done.

--- index.js
+++ index.js
@@ -12,18 +12,52 @@ function next(node, collection) {
 }
 
 function nodeAt(index, linkedList, collection) {
+    let currentNode = headNode(linkedList, collection)
+    for(let i = 0; i < index; i++) {
+        currentNode = next(currentNode, collection)
+    }
     collection[index]
 }
 
-function addressAt(index, linkedList, collection) {
-
-}
-
-function indexAt(node, collection, linkedList) {
-    for(i = 0; i < index; i++) {
-        node = next(node, collection)
+function addressAt(index, linkedList, collection){
+    if(index === 0){
+      return linkedList
+    } else {
+      let node = nodeAt(index-1, linkedList, collection)
+      return node.next
     }
+  }
 
-    return node
-}
+  function indexAt(node, collection, linkedList){
+    let currentNode = headNode(linkedList, collection);
+    let currentIndex = 0
+    while(currentNode != node){
+        currentIndex++
+      currentNode = next(currentNode, collection)
+    }
+    return currentIndex
+  }
 
+//   function insertNodeAt(index, newNodeAddress, linkedList, collection){
+//     let previousNode = nodeAt(index - 1, linkedList, collection)
+//     let subsequentNode = nodeAt(index, linkedList, collection)
+  
+//     let previousNodeIdx = indexAt(previousNode, collection, linkedList)
+//     let subsequentNodeIdx = indexAt(subsequentNode, collection, linkedList)
+//     let previousNodeAddress = addressAt(previousNode, linkedList, collection)
+//     let subsequentNodeAddress = addressAt(subsequentNode, linkedList, collection)
+//     previousNode.next = newNodeAddress
+//     let newNode = collection[newNodeAddress]
+//     newNode.next = subsequentNodeAddress
+//   }
+  
+  
+  function deleteNodeAt(index, linkedList, collection){
+    let previousNode;
+    let currentNode = headNode(linkedList, collection);
+    for(let i = 0; i < index; i++){
+       previousNode = currentNode
+       currentNode = next(currentNode, collection);
+    }
+    previousNode.next = currentNode.next
+  }
