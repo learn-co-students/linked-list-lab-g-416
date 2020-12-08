3034f6d310a0ceebb51234b71da0b0473f3aec26
Wendy Harris <wendyv510@hotmail.com>
2020-12-08 11:09:10 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function getName(name){
+
+}
+
+function next(node){
+    let nextAddress = node[1] 
+    return collection[nextAddress]
+}
+
+function nodeAt(){
+
+}
+
+function addressAt(){
+
+}
+
+function indexAt(head,index){
+    let node = head; 
+    for(i = 0; i < index; i++){ 
+        node = next(node)
+    }
+    return node 
+}
+
+function insertNodeAt(){
+
+}
+
+function deleteNodeAt(){
+    
+}
+
