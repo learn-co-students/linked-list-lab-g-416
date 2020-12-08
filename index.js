134f7f08a7ea2aa0d80354de7d0206790a6c026f
Wendy Harris <wendyv510@hotmail.com>
2020-12-08 12:31:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,58 @@
-function getName(name){
-
+function getName(node){
+    return node.name
 }
 
-function next(node){
-    let nextAddress = node[1] 
-    return collection[nextAddress]
+function headNode(headKey,list){
+    return list[headKey]
 }
 
-function nodeAt(){
-
+function next(node, list){
+    return !!node ? list[node.next] : null 
 }
 
-function addressAt(){
-
-}
-
-function indexAt(head,index){
-    let node = head; 
-    for(i = 0; i < index; i++){ 
-        node = next(node)
+function nodeAt(index, headKey, list){
+    let node = headNode(headKey, list) 
+    for(let i = 0; i < index; i++){
+        node = next(node,list)
+        if(!node) return null 
     }
     return node 
 }
 
-function insertNodeAt(){
-
+function addressAt(index, headKey,list){
+    if(index === 0) return headKey 
+    const prevNode = nodeAt(index -1, headKey,list)
+    return !!prevNode ? prevNode.next : null 
 }
 
-function deleteNodeAt(){
-    
+function indexAt(node, list, headKey){
+    let currentNode = list[headKey]
+    let counter = 0 
+    while(node !== currentNode){
+        currentNode = next(currentNode,list)
+        counter++ 
+        if(!currentNode) return -1 
+    }
+    return counter 
+}
+
+function insertNodeAt(index, newNodeKey, headKey,list){
+    list[newNodeKey].next = addressAt(index,headKey,list)
+    if(index > 0){
+        const prevNode = nodeAt(index -1, headKey,list)
+        if(!prevNode) throw 'The index is out of bounds of the list'
+        prevNode.next = newNodeKey 
+    } else { 
+        headkey = newNodeKey
+    }
+}
+
+function deleteNodeAt(index, headKey,list){
+    if(index === 0){
+        headKey = headNode(headKey,list).next
+    }
+    const prevNode = nodeAt(index -1, headKey, list)
+    if(!prevNode) throw 'The index is out of bounds of the list' 
+    prevNode.next = addressAt(index + 1, headKey, list)
 }
 
