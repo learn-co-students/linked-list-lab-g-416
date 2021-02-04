b592c4d0aff6ff31ae6cb743f7672c8d853c3d3f
ViolaCello <ViolaCelloNYC@gmail.com>
2021-02-04 12:36:20 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,59 @@
+function getName(node) {
+    return node["name"]
+}
+
+function headNode(head, ll) {
+    return ll[head]
+}
+
+function next(node, ll) {
+    let pointer = node["next"]
+    return ll[pointer]
+}
+
+function nodeAt(index,b,c) {
+  
+    let head = headNode(b,c)
+    for (let i=0; i<index; i++) {
+        console.log("i=", i, " head=",head)
+        head = next(head, c)
+    }
+    return head
+}
+
+function addressAt(a,b,c) {
+    if (a==0) {
+        return b
+    } else 
+    {
+        let node = nodeAt(a-1,b,c)
+    return node["next"]
+    }
+}
+
+function indexAt(address, collection, head) {
+    let index = 0
+    let match = collection[head]
+    while (address != match) {
+        match = next(match, collection)
+        index ++
+    }
+    return index
+//    if (address==collection[head]) {
+//        return 0
+//    } else {
+//         let match=0;
+//         let index = 1;
+//         let n = collection[head]
+//         while (match==0) {
+//              n = next(n, collection)
+//             console.log("n=",n," address=",address)
+//             if (address==n) {
+//                 match = 1
+//                 return index
+//             } else {
+//                 index ++
+//             }
+//         }
+//    }
+}
